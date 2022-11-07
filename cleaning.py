88ca6e4edce6e0428b75bb7db50c10243341c88f
MeghanaGantla <meghana_gantla@gwu.edu>
2022-11-07 17:22:51 -0500

    cleaning 1

---
+++ cleaning.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv("US_Accidents_Dec21_updated.csv")
+
+
+df.drop(["ID", "End_Lat", "End_Lng", "Description", "Side", "Zipcode", "Country", "Weather_Timestamp", "Civil_Twilight",
+         "Nautical_Twilight", "Astronomical_Twilight"], axis=1)
+print(df.columns)
